6b9284e184651129b4226df37f04008c578beba4
Ren Wang <iamwrm@gmail.com>
2024-03-13 15:14:15 +0800

    try run

--- scripts2/0_download.sh
+++ scripts2/0_download.sh
@@ -1,3 +1,5 @@
 curl -OL https://fastdl.mongodb.org/tools/mongosync/mongosync-ubuntu2004-x86_64-1.7.1.tgz
 curl -OL https://downloads.mongodb.com/compass/mongosh-2.1.5-linux-x64.tgz
 
+tar xvf mongosync-ubuntu2004-x86_64-1.7.1.tgz 
+tar xvf mongosh-2.1.5-linux-x64.tgz
