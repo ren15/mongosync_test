757e24b5acb384fe52a2e732fe6fabad317f4b45
Ren Wang <iamwrm@gmail.com>
2024-03-13 15:17:36 +0800

    add send

--- scripts2/3_start_mongosync.sh
+++ scripts2/3_start_mongosync.sh
@@ -1,4 +1,7 @@
+mkdir -p ./logs
+
 ./mongosync-ubuntu2004-x86_64-1.7.1/bin/mongosync \
+    --logPath ./logs \
     --cluster0 "mongodb://127.0.0.1:27017/?replicaSet=rs0&directConnection=true" \
     --cluster1 "mongodb://127.0.0.1:27018/?replicaSet=rs0&directConnection=true" 
     
